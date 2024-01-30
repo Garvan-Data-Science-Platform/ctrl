d5abbe3a7004a1b09c2861813903b7d67fc5a91b
ignatiusm <11403879+ignatiusm@users.noreply.github.com>
2024-01-30 19:27:32 +1300

    Feature/ctrl 71 add linting (#8)

--- application/backend/src/index.ts
+++ application/backend/src/index.ts
@@ -1,20 +1,20 @@
-import cors from 'cors'
-import express from 'express'
+import cors from "cors";
+import express from "express";
 
-import { Workspace } from 'common'
+import { Workspace } from "common";
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get('/workspaces', (_, reponse) => {
+app.get("/workspaces", (_, reponse) => {
   const workspaces: Workspace[] = [
-    { name: 'backend', version: '1.0.0' },
-    { name: 'common', version: '1.0.0' },
-    { name: 'frontend', version: '1.0.0' },
-  ]
-  reponse.json({ data: workspaces })
-})
+    { name: "backend", version: "1.0.0" },
+    { name: "common", version: "1.0.0" },
+    { name: "frontend", version: "1.0.0" },
+  ];
+  reponse.json({ data: workspaces });
+});
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
