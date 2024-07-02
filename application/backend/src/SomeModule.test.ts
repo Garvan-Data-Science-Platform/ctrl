b2fcad533d7b0a46dc2179b41b63c321f8714ad3
Peter Louka <p.louka13@gmail.com>
2024-07-02 12:49:49 +1000

    Adds testing for stub module

---
+++ application/backend/src/SomeModule.test.ts
@@ -0,0 +1,20 @@
+import { add, subtract } from "./SomeModule";
+
+describe("Add", () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(-1, 1)).toBe(0);
+    expect(add(0, 0)).toBe(0);
+    expect(add(1.5, 2.5)).toBe(4);
+  })
+});
+
+describe("Subtract", () => {
+  it('should subtract two numbers', () => {
+    expect(subtract(1, 2)).toBe(-1);
+    expect(subtract(-1, 1)).toBe(-2);
+    expect(subtract(0, 0)).toBe(0);
+    expect(subtract(1.5, 2.5)).toBe(-1);
+    expect(subtract(5, 1)).toEqual(-subtract(1, 5));
+  })
+});
