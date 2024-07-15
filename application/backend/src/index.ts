91d6a8caa8ddd9490bd04ab9ebe7cf8639faf746
Peter Louka <p.louka13@gmail.com>
2024-07-15 10:07:12 +1000

    fix: reimplement index to be a main function that spins up server and config

--- application/backend/src/index.ts
+++ application/backend/src/index.ts
@@ -1,27 +1,11 @@
-import cors from 'cors'
-import express from 'express'
-
-import { backendPort, Workspace } from 'common'
-
-const app = express()
-
-app.use(cors({ origin: 'http://localhost:3000' }))
-
-app.get('/workspaces', (_, response) => {
-  const workspaces: Workspace[] = [
-    { name: 'backend', version: '1.0.0' },
-    { name: 'common', version: '1.0.0' },
-    { name: 'frontend', version: '1.0.0' },
-  ]
-  response.json({ data: workspaces })
-})
-
-app.listen(backendPort, () => console.log(`Listening on http://localhost:${backendPort}`))
+import { WorkspacesRouter } from './routes/WorkspacesRouter'
+import { Api } from './Api'
+import { SomeModuleRouter } from './routes/SomeModuleRouter'
 
 const main = async (): Promise<void> => {
-  // Setup DB
-  // Setup other config
   // Setup and run Api
+  const api = new Api(WorkspacesRouter(), SomeModuleRouter())
+  api.run()
 }
 
 main()
