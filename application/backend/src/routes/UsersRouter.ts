5b4166b82fbb588ab4d842384bf012f9cde36a65
Peter Louka <p.louka13@gmail.com>
2024-07-18 09:24:50 +1000

    chore: Fixes delete endpoint and adds basic logging

--- application/backend/src/routes/UsersRouter.ts
+++ application/backend/src/routes/UsersRouter.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router } from 'express'
 import { User } from 'src/entities/User'
 
+/* TODO: Remove when implementing DB
+ ** - need to remove the refs to users list in each endpoint
+ */
 const users: User[] = []
 let nextId = 1
 
@@ -8,32 +11,32 @@ export const UsersRouter = (): Router => {
   const router: Router = Router()
 
   router.get('/', async (req: Request, res: Response) => {
-    console.log(users)
+    console.log({ msg: 'Get all users', users })
     res.status(200).json({ data: users })
   })
 
   router.get('/:id', async (req: Request, res: Response) => {
     const user = users.find((user) => user.id === parseInt(req.params.id, 10))
-    console.log(user)
+    console.log({ msg: `Get user w/ ID: ${req.params.id}`, user })
     res.status(200).json({ data: user })
   })
 
   router.post('/', async (req: Request, res: Response) => {
-    const user = new User(
+    const newUser = new User(
       nextId++,
       req.body.name,
       req.body.email,
       req.body.role,
       req.body.organisations,
     )
-    users.push(user)
-    res.status(200).json({ data: user })
+    console.log({ msg: `Creates user`, newUser })
+    users.push(newUser)
+    res.status(200).json({ data: newUser })
   })
 
-  router.delete('/', async (req: Request, res: Response) => {
-    const updatedUsers = users.filter((user) => user.id !== parseInt(req.body.id, 10))
-    users.length = 0
-    users.push(...updatedUsers)
+  router.delete('/:id', async (req: Request, res: Response) => {
+    const updatedUsers = users.filter((user) => user.id !== parseInt(req.params.id, 10))
+    console.log({ msg: `Delete user w/ ID: ${req.params.id}`, updatedUsers })
     res.status(200).json({ data: updatedUsers })
   })
 
