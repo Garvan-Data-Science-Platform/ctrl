26ca36848f5b87c8ca8241770f5025794845d7ba
Peter Louka <p.louka13@gmail.com>
2024-07-25 15:47:38 +1000

    chore: adds .env to backend

--- application/backend/src/index.ts
+++ application/backend/src/index.ts
@@ -3,6 +3,9 @@ import { Api } from './Api'
 import { SomeModuleRouter } from './routes/SomeModuleRouter'
 import { UsersRouter } from './routes/UsersRouter'
 import Database from './Database'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../../.env' })
 
 const main = async (): Promise<void> => {
   // Setup Database
