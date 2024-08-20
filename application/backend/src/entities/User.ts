8b7c0450348dd025169d8bbc49549669579e3812
Peter Louka <p.louka13@gmail.com>
2024-08-20 16:06:13 +1000

    chore: don't need database.ts and user.ts entity anymore

--- application/backend/src/entities/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  firstName: string
-  lastName: string
-  email: string
-  role: string
-  organisations: string[] // TODO: Change type to Organisation once implemented
-  createdAt: Date
-  updatedAt: Date
-}
