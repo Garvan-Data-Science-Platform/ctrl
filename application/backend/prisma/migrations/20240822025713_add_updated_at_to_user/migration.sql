e73dc52212906a70d7f0417f1d60d84187cf009c
Peter Louka <p.louka13@gmail.com>
2024-08-22 13:00:00 +1000

    chore: updatedAt should be createdAt if null

--- application/backend/prisma/migrations/20240822025713_add_updated_at_to_user/migration.sql
+++ application/backend/prisma/migrations/20240822025713_add_updated_at_to_user/migration.sql
@@ -1,8 +1,6 @@
 -- AlterTable
-
 ALTER TABLE "User" ADD COLUMN "updatedAt" TIMESTAMP(3);
 
-UPDATE "User" SET "updatedAt" = NOW() WHERE "updatedAt" IS NULL;
+UPDATE "User" SET "updatedAt" = COALESCE("updatedAt", "createdAt");
 
 ALTER TABLE "User" ALTER COLUMN "updatedAt" SET NOT NULL;
-
