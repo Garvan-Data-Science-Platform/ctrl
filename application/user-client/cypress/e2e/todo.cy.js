ca4e89e128eb71f716ec87be06f2e96f1c06d2f1
Tim Kallady <t.kallady@garvan.org.au>
2024-09-18 13:29:42 +1000

    Fix e2e test

--- application/user-client/cypress/e2e/todo.cy.js
+++ application/user-client/cypress/e2e/todo.cy.js
@@ -3,6 +3,6 @@
 describe('example to-do app', () => {
   it('renders homepage', () => {
     cy.visit('/')
-    cy.contains('My Acativities').should('exist')
+    cy.contains('My Activities').should('exist')
   })
 })
