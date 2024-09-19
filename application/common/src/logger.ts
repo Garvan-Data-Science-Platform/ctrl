d83f8bed62dd54245fd3e6ad44c8ebfd776f4a20
Peter Louka <58866918+plouka13@users.noreply.github.com>
2024-09-19 11:40:05 +1000

    Feature/52 coverage reports in ci (#92)

--- application/common/src/logger.ts
+++ application/common/src/logger.ts
@@ -5,6 +5,7 @@ const logger = winston.createLogger({
   level: 'info',
   format: combine(errors({ stack: true }), timestamp(), json(), prettyPrint()),
   transports: [new transports.Console()],
+  silent: process.env.NODE_ENV === 'test',
 })
 
 export default logger
