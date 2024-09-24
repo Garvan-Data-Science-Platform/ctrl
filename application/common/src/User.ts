197292e3c7d10f6f06cafff075f8d53d6e084eaf
Tim Kallady <t.kallady@garvan.org.au>
2024-09-24 15:28:21 +1000

    Change structure

--- application/common/src/User.ts
+++ application/common/src/User.ts
@@ -7,21 +7,9 @@ export interface UserCreationRequest {
   role: string
 }
 
-export interface UserUpdateRequest {
-  firstName?: string
-  lastName?: string
-  email?: string
-  role?: string
-}
-
 export interface GetAllUsersResponse {
   message: string
   users: User[]
-}
-
-export interface GetUserByIdResponse {
-  message: string
-  user: User | null
 }
 
 export interface CreateUserResponse {
@@ -29,62 +17,7 @@ export interface CreateUserResponse {
   newUser: User | null
 }
 
-export interface UpdateUserResponse {
-  message: string
-  updatedUser: User | null
-}
-
 export interface DeleteUserResponse {
   message: string
   deletedUser: User | null
-}
-
-export enum ContactMethod {
-  mobile = 'mobile',
-  email = 'email',
-  mail = 'mail',
-}
-
-export enum StateTerritory {
-  ACT = 'act',
-  NSW = 'nsw',
-  NT = 'nt',
-  QLD = 'qld',
-  SA = 'sa',
-  TAS = 'tas',
-  VIC = 'vic',
-  WA = 'wa',
-}
-
-export interface AlternativeContact {
-  firstName: string
-  middleName?: string
-  lastName: string
-  phone: string
-  email: string
-}
-
-export interface OnBehalf {
-  firstName: string
-  middleName?: string
-  lastName: string
-  dob: string
-}
-
-export interface UserProfile {
-  firstName: string
-  middleName?: string
-  lastName: string
-  dob: string
-  participantID: string
-  email: string
-  mobile: string
-  addressLine?: string
-  suburb?: string
-  state?: StateTerritory
-  postcode?: string
-  preferredContact: ContactMethod
-  isParentOrGuardian: boolean
-  alternativeContact?: AlternativeContact
-  onBehalfOf?: OnBehalf
 }
