29c934ca7ea890b554b8af96e787933890942557
Tim Kallady <t.kallady@garvan.org.au>
2024-10-16 11:54:13 +1100

    Add test db and admin client boilerjplate

--- application/backend/jest.config.js
+++ application/backend/jest.config.js
@@ -1,4 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
--- application/backend/scripts/integration_tests.sh
+++ application/backend/scripts/integration_tests.sh
@@ -1,14 +1,10 @@
 # Spin-up db
-docker compose --env-file .env up -d
-
-# Wait for db
-echo "Waiting for db to spin up..."
-sleep 5
+docker compose up -d db-test --wait
 
 # migrate db
-yarn prisma migrate deploy
+dotenv -e .env.test -- yarn prisma migrate deploy
 
 # run tests
-jest --detectOpenHandles --coverage
+dotenv -e .env.test -- jest --detectOpenHandles --coverage
 
-docker compose down
+docker compose down db-test
