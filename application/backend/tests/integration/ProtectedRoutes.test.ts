e8887aec75a676632495dc90634a78a7eaafbe0e
Peter Louka <p.louka13@gmail.com>
2024-10-22 11:36:30 +1100

    cleanup test structure

--- application/backend/tests/integration/ProtectedRoutes.test.ts
+++ application/backend/tests/integration/ProtectedRoutes.test.ts
@@ -11,7 +11,7 @@ enum HttpMethod {
 const api = new Api()
 const app = api.app
 
-describe('Api', () => {
+describe('Protected Routes', () => {
   beforeAll(async () => {
     api.run()
   })
@@ -20,46 +20,44 @@ describe('Api', () => {
     api.stop()
   })
 
-  describe('Protected Routes', () => {
-    const checkProtectedRoute = async (method: HttpMethod, url: string) => {
-      const response = await request(app)[method](url)
-      expect(response.status).toBe(401)
+  const checkProtectedRoute = async (method: HttpMethod, url: string): Promise<void> => {
+    const response = await request(app)[method](url)
+    expect(response.status).toBe(401)
 
-      const body = response.body
-      expect(body.message).toBe('No token provided')
-    }
+    const body = response.body
+    expect(body.message).toBe('No token provided')
+  }
 
-    const userRoutes = [
-      { method: HttpMethod.GET, url: '/users' },
-      { method: HttpMethod.GET, url: '/users/1' },
-      { method: HttpMethod.PATCH, url: '/users/1' },
-      { method: HttpMethod.DELETE, url: '/users/1' },
-    ]
+  const userRoutes = [
+    { method: HttpMethod.GET, url: '/users' },
+    { method: HttpMethod.GET, url: '/users/1' },
+    { method: HttpMethod.PATCH, url: '/users/1' },
+    { method: HttpMethod.DELETE, url: '/users/1' },
+  ]
 
-    const organisationRoutes = [
-      { method: HttpMethod.GET, url: '/organisations' },
-      { method: HttpMethod.GET, url: '/organisations/1' },
-      { method: HttpMethod.POST, url: '/organisations' },
-      { method: HttpMethod.PATCH, url: '/organisations/1' },
-      { method: HttpMethod.DELETE, url: '/organisations/1' },
-      { method: HttpMethod.GET, url: '/organisations/1/users' },
-      { method: HttpMethod.POST, url: '/organisations/1/users/2' },
-      { method: HttpMethod.DELETE, url: '/organisations/1/users/2' },
-    ]
+  const organisationRoutes = [
+    { method: HttpMethod.GET, url: '/organisations' },
+    { method: HttpMethod.GET, url: '/organisations/1' },
+    { method: HttpMethod.POST, url: '/organisations' },
+    { method: HttpMethod.PATCH, url: '/organisations/1' },
+    { method: HttpMethod.DELETE, url: '/organisations/1' },
+    { method: HttpMethod.GET, url: '/organisations/1/users' },
+    { method: HttpMethod.POST, url: '/organisations/1/users/2' },
+    { method: HttpMethod.DELETE, url: '/organisations/1/users/2' },
+  ]
 
-    describe('User Routes', () => {
-      userRoutes.forEach(({ method, url }) => {
-        it(`${method.toUpperCase()} ${url} should be a protected route`, async () => {
-          await checkProtectedRoute(method, url)
-        })
+  describe('User Routes', () => {
+    userRoutes.forEach(({ method, url }) => {
+      it(`${method.toUpperCase()} ${url} should be a protected route`, async () => {
+        await checkProtectedRoute(method, url)
       })
     })
+  })
 
-    describe('Organisation Routes', () => {
-      organisationRoutes.forEach(({ method, url }) => {
-        it(`${method.toUpperCase()} ${url} should be a protected route`, async () => {
-          await checkProtectedRoute(method, url)
-        })
+  describe('Organisation Routes', () => {
+    organisationRoutes.forEach(({ method, url }) => {
+      it(`${method.toUpperCase()} ${url} should be a protected route`, async () => {
+        await checkProtectedRoute(method, url)
       })
     })
   })
