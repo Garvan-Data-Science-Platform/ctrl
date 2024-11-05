e05fbcd43f1c630a2d0d95ed4fb303e9b06251be
Peter Louka <p.louka13@gmail.com>
2024-11-05 12:11:43 +1100

    fix: update schema

---
+++ application/backend/prisma/migrations/20241104233843_add_profile/migration.sql
@@ -0,0 +1,54 @@
+-- CreateEnum
+CREATE TYPE "RelationshipType" AS ENUM ('PARENT', 'GUARDIAN', 'CHILD', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "StateTerritory" AS ENUM ('ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA');
+
+-- CreateEnum
+CREATE TYPE "ContactMethod" AS ENUM ('EMAIL', 'MOBILE', 'MAIL');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "middleName" TEXT;
+
+-- CreateTable
+CREATE TABLE "ParticipantProfile" (
+    "id" SERIAL NOT NULL,
+    "dob" DATE NOT NULL,
+    "participantID" TEXT NOT NULL,
+    "mobile" TEXT NOT NULL,
+    "addressLine" TEXT,
+    "suburb" TEXT,
+    "state" "StateTerritory",
+    "postcode" TEXT,
+    "isParentOrGuardian" BOOLEAN NOT NULL,
+    "preferredContact" "ContactMethod" NOT NULL,
+    "userID" INTEGER NOT NULL,
+
+    CONSTRAINT "ParticipantProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AlternativeContact" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "middleName" TEXT,
+    "lastName" TEXT NOT NULL,
+    "mobile" TEXT,
+    "email" TEXT NOT NULL,
+    "relationship" "RelationshipType" NOT NULL,
+    "participantProfileId" INTEGER NOT NULL,
+
+    CONSTRAINT "AlternativeContact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ParticipantProfile_userID_key" ON "ParticipantProfile"("userID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AlternativeContact_participantProfileId_key" ON "AlternativeContact"("participantProfileId");
+
+-- AddForeignKey
+ALTER TABLE "ParticipantProfile" ADD CONSTRAINT "ParticipantProfile_userID_fkey" FOREIGN KEY ("userID") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AlternativeContact" ADD CONSTRAINT "AlternativeContact_participantProfileId_fkey" FOREIGN KEY ("participantProfileId") REFERENCES "ParticipantProfile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
