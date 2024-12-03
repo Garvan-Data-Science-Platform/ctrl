d2f1719640b61c002836f2f3f251d99504e21d74
Rishi <z5420471>
2024-12-03 12:21:11 +1100

    setup yarn workspaces for integrations

---
+++ application/integrations/src/Integrations.ts
@@ -0,0 +1,5 @@
+import logger from 'common/src/logger'
+
+export class Integrations {
+    // a series of methods for integrations
+}
---
+++ application/integrations/src/index.ts
@@ -0,0 +1,1 @@
+// TODO: add exports for integration functions from class
