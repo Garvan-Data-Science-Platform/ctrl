2cf10b94ad3b35d930345889882b45d056ace27c
Peter Louka <p.louka13@gmail.com>
2024-12-05 09:48:15 +1100

    chore: shouldn't be a default here?

--- application/backend/src/controllers/MailerController.ts
+++ application/backend/src/controllers/MailerController.ts
@@ -59,8 +59,8 @@ export class MailerController extends Controller {
     }
 
     const transporter = nodemailer.createTransport({
-      host: process.env.MAILER_HOST || 'smtp.gmail.com',
-      port: Number(process.env.MAILER_PORT) || 465,
+      host: process.env.MAILER_HOST,
+      port: Number(process.env.MAILER_PORT),
       auth: {
         user: process.env.MAILER_USERNAME,
         pass: process.env.MAILER_PASSWORD,
