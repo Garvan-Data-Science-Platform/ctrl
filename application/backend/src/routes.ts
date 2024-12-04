032a2e5264f7c91962297ba5bb20899e264ce910
Peter Louka <p.louka13@gmail.com>
2024-12-04 16:00:35 +1100

    chore: build docs

--- application/backend/src/routes.ts
+++ application/backend/src/routes.ts
@@ -510,6 +510,14 @@ const models: TsoaRoute.Models = {
         "additionalProperties": false,
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "ContactUsResponse": {
+        "dataType": "refObject",
+        "properties": {
+            "message": {"dataType":"string","required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     "ContactUsRequest": {
         "dataType": "refObject",
         "properties": {
