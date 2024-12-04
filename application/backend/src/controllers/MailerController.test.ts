fffc49e0092a9221281fc2ffe8ad5db20c52ccfe
Peter Louka <p.louka13@gmail.com>
2024-12-04 16:26:09 +1100

    feature: test init for mailer

---
+++ application/backend/src/controllers/MailerController.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { Api } from '../Api'
+import { generateToken } from '../authentication'
+import { resetDB } from '../../tests/TestHelpers'
+import { Role } from '@prisma/client'
+
+const api = new Api()
+const app = api.app
+
+describe('MailerController', () => {
+  let participantToken: string
+  const registeredParticipantUserID: number = 99
+
+  beforeAll(async () => {
+    participantToken = await generateToken({
+      userId: registeredParticipantUserID,
+      roles: [Role.Participant],
+    })
+
+    api.run()
+  })
+
+  beforeEach(async () => {
+    await resetDB()
+  })
+
+  afterAll(async () => {
+    api.stop()
+  })
+
+  describe('POST /mailer/contact-us', () => {
+    it('should', async () => {
+      const response = await request(app)
+        .post('/mailer/contact-us')
+        .send({ subject: 'Test Subject', content: 'Test Content' })
+        .set({ Authorization: `Bearer ${participantToken}` })
+
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({
+        message: 'Contact us request successfully sent to admin team.',
+      })
+    }, 100000)
+  })
+})
