05f3ae50d3f02defc88a461876203077be99d65e
Tim Kallady <t.kallady@garvan.org.au>
2024-12-10 13:03:07 +1100

    Only check db if env vars are defined

--- application/backend/src/utils/createAdmin.ts
+++ application/backend/src/utils/createAdmin.ts
@@ -2,18 +2,19 @@ import { hashPassword } from '../authentication'
 import prisma from '../PrismaClient'
 
 const createAdmin = async () => {
-  const admin = await prisma.user.findFirst({ where: { email: process.env['ADMIN_EMAIL'] } })
-
-  if (!admin && process.env['ADMIN_PASSWORD'] && process.env['ADMIN_EMAIL']) {
-    await prisma.user.create({
-      data: {
-        email: process.env['ADMIN_EMAIL'] as string,
-        firstName: 'ADMIN',
-        lastName: 'USER',
-        password: await hashPassword(process.env['ADMIN_PASSWORD']),
-        role: 'OrganisationAdmin',
-      },
-    })
+  if (process.env['ADMIN_PASSWORD'] && process.env['ADMIN_EMAIL']) {
+    const admin = await prisma.user.findFirst({ where: { email: process.env['ADMIN_EMAIL'] } })
+    if (!admin) {
+      await prisma.user.create({
+        data: {
+          email: process.env['ADMIN_EMAIL'] as string,
+          firstName: 'ADMIN',
+          lastName: 'USER',
+          password: await hashPassword(process.env['ADMIN_PASSWORD']),
+          role: 'OrganisationAdmin',
+        },
+      })
+    }
   }
 }
 
