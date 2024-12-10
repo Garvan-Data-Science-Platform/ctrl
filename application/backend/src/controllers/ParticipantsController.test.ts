f703d40ccb7e4dfadf3f299171be4af8c9eb9c32
Peter Louka <p.louka13@gmail.com>
2024-12-10 14:13:41 +1100

    fix: tests

--- application/backend/src/controllers/ParticipantsController.test.ts
+++ application/backend/src/controllers/ParticipantsController.test.ts
@@ -38,7 +38,7 @@ describe('ParticipantsController', () => {
       console.log('BODY', body)
       expect(body.data).toHaveLength(2)
       expect(body.data[0]).not.toHaveProperty('lastUpdated')
-      expect(['03/12/2024', '02/12/2024']).toContain(body.data[1].lastUpdated)
+      expect(['03/12/2024', '12/2/2024']).toContain(body.data[1].lastUpdated)
       expect(body.data[1].answers).toHaveLength(1)
       expect(body.data[1].answers[0].status).toBe('incomplete')
     })
