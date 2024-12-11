74a470268f49f82689ea988da9bb8c52cc26c93c
Rishi <z5420471>
2024-12-11 23:06:08 +1100

    fixed tests

--- application/backend/src/controllers/IntegrationsController.test.ts
+++ application/backend/src/controllers/IntegrationsController.test.ts
@@ -74,7 +74,7 @@ describe('IntegrationsController', () => {
                 .set({ Authorization: `Bearer ${token}` })
             expect(response.status).toBe(404)
             const body: UploadRedCapParticipantsResponse = response.body
-            expect(body.message).toBe('File is empty')
+            expect(body.message).toBe('No file uploaded')
         })
 
         it('should throw a 404 error if given an empty file', async () => {
@@ -85,7 +85,7 @@ describe('IntegrationsController', () => {
                 .attach('file', csvPath); 
             expect(response.status).toBe(404)
             const body: UploadRedCapParticipantsResponse = response.body
-            expect(body.message).toBe('Not Found')
+            expect(body.message).toBe('File is empty')
         })
 
         it('should throw an error if the file is not a csv', async () => {
