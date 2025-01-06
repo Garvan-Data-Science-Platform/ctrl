ca9e10c767a081ed6d94f0dbac4f056e00c09427
Tim Kallady <t.kallady@garvan.org.au>
2025-01-06 14:50:54 +1100

    Get cypress working with dockerised backend

--- application/user-client/cypress.config.ts
+++ application/user-client/cypress.config.ts
@@ -2,12 +2,16 @@ import { resetDB } from 'common/testing/TestHelpers'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  env: {
+    DATABASE_URL: 'postgres://postgres:password@db-test:5432/ctrl',
+  },
   e2e: {
+    baseUrl: 'http://localhost:5173',
     // eslint-disable-next-line
     setupNodeEvents(on, config) {
       on('task', {
         reset({}) {
-          resetDB()
+          return resetDB()
         },
       })
     },
