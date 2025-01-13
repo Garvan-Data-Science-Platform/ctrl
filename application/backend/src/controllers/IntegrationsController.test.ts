8d2b114d8269430380e18103be7e7cdbd6294647
Rishi <z5420471>
2025-01-14 00:58:10 +1100

    test fix

--- application/backend/src/controllers/IntegrationsController.test.ts
+++ application/backend/src/controllers/IntegrationsController.test.ts
@@ -46,7 +46,7 @@ describe('IntegrationsController', () => {
     it('should register multiple users from one csv', async () => {
       const initialLen = await prisma.participantProfile.count()
 
-      const csvPath = path.join(TESTS_PATH, '90_users.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/90_users.csv')
       const response = await request(app)
         .post('/integrations/redcap/participant/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -71,7 +71,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw a 404 error if given an empty file', async () => {
-      const csvPath = path.join(TESTS_PATH, 'empty_file.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/empty_file.csv')
       const response = await request(app)
         .post('/integrations/redcap/participant/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -82,7 +82,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw an error if the file is not a csv', async () => {
-      const csvPath = path.join(TESTS_PATH, 'not_a_csv.txt')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/not_a_csv.txt')
       const response = await request(app)
         .post('/integrations/redcap/participant/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -93,7 +93,7 @@ describe('IntegrationsController', () => {
 
   describe('POST integrations/redcap/instrument/upload', () => {
     it('should update a valid draft survey from instrument csv', async () => {
-      const csvPath = path.join(TESTS_PATH, 'instrument.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/instrument.csv')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -137,7 +137,7 @@ describe('IntegrationsController', () => {
       // In the seed data the second seed is the draft survey - we remove it to test creating a new survey
       await prisma.surveyVersion.update({ where: { id: 2 }, data: { status: 'PUBLISHED' } })
 
-      const csvPath = path.join(TESTS_PATH, 'instrument.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/instrument.csv')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -162,7 +162,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw a 404 error if given an empty file', async () => {
-      const csvPath = path.join(TESTS_PATH, 'empty_file.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/empty_file.csv')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -172,7 +172,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw a 400 error if the file is not a csv', async () => {
-      const csvPath = path.join(TESTS_PATH, 'not_a_csv.txt')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/not_a_csv.txt')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -181,7 +181,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw a 400 error if the csv contents have a missing key header', async () => {
-      const csvPath = path.join(TESTS_PATH, 'missing_key_header.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/missing_key_header.csv')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -191,7 +191,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw a 400 error if any record has a missing key field', async () => {
-      const csvPath = path.join(TESTS_PATH, 'one_missing_field.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/one_missing_field.csv')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
@@ -204,7 +204,7 @@ describe('IntegrationsController', () => {
     })
 
     it('should throw a 400 error if a required field is empty', async () => {
-      const csvPath = path.join(TESTS_PATH, 'one_empty_field.csv')
+      const csvPath = path.resolve(__dirname, '../../tests/test_data/one_empty_field.csv')
       const response = await request(app)
         .post('/integrations/redcap/instrument/upload')
         .set({ Authorization: `Bearer ${token}` })
