2e2b9994ff441cb26fcae708991346da89a23b05
Rishi <z5420471>
2025-01-14 23:58:25 +1100

    utilised upsert and made subheadings into steps

--- application/backend/src/controllers/IntegrationsController.ts
+++ application/backend/src/controllers/IntegrationsController.ts
@@ -139,46 +139,30 @@ export class IntegrationsController extends Controller {
     // fetches elements from mapping
     let elements: SurveyElement[] = []
     try {
-      elements = this.integrationService.mapInstrumentCSVToSurvey(csvData)
+      steps = this.integrationService.mapInstrumentCSVToSurvey(csvData)
     } catch (error) {
       throw new FileUploadError(
         error instanceof Error ? error.message : 'Unknown Error: Failed to Map Data',
       )
     }
 
-    const steps: SurveyStep[] = [
-      {
-        title: 'Imported Survey',
-        text: 'Survey Imported from Redcap Instrument',
-        elements: elements,
-      },
-    ]
-
-    // Check if a draft survey already exists
     const existingSurvey = await this.surveyRepo.findFirst({
       where: { status: 'DRAFT' },
     })
 
-    let survey
-    if (existingSurvey) {
-      // Update the existing survey
-      survey = await this.surveyRepo.update({
-        where: { id: existingSurvey.id },
-        data: {
-          versionNumber: existingSurvey.versionNumber + 1,
-          data: steps,
-        },
-      })
-    } else {
-      // Create a new survey
-      survey = await this.surveyRepo.create({
-        data: {
-          status: 'DRAFT',
-          versionNumber: 1,
-          data: steps,
-        },
-      })
-    }
+    // prisma doesn't let you use where to find a non-unique id here so we have to use find first in the previous ine
+    const survey = await this.surveyRepo.upsert({
+      where: { id: existingSurvey ? existingSurvey.id : -1 }, // Use a non-existent id for creation
+      update: {
+        versionNumber: { increment: 1 },
+        data: steps,
+      },
+      create: {
+        status: 'DRAFT',
+        versionNumber: 1,
+        data: steps,
+      },
+    })
 
     return { id: survey.id }
   }
