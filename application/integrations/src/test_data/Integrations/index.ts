f2fa2de2c984faec39c157e2350d2db55239955a
Rishi <z5420471>
2025-01-21 13:39:35 +1100

    added index.ts

---
+++ application/integrations/src/test_data/Integrations/index.ts
@@ -0,0 +1,19 @@
+import ansMultipleProfilesMapping from './ansMultipleProfilesMapping.json'
+import exampleSingleProfile from './exampleSingleProfile.json'
+import exampleMultipleProfiles from './exampleMultipleProfiles.json'
+import ansSingleProfile from './ansSingleProfile.json'
+import exampleQuestionDataHeader from './exampleQuestionDataHeader.json'
+import exampleQuestionDataNoHeader from './exampleQuestionDataNoHeader.json'
+import ansQuestionDataHeader from './ansQuestionDataHeader.json'
+import ansQuestionDataNoHeader from './ansQuestionDataNoHeader.json'
+
+export {
+  exampleSingleProfile,
+  exampleMultipleProfiles,
+  exampleQuestionDataNoHeader,
+  exampleQuestionDataHeader,
+  ansSingleProfile,
+  ansMultipleProfilesMapping,
+  ansQuestionDataHeader,
+  ansQuestionDataNoHeader,
+}
