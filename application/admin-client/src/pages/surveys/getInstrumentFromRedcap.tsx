7081b33e309ac4495e37fe395242ae6c1eddd8b3
Peter Louka <p.louka13@gmail.com>
2025-03-07 13:30:54 +1100

    import instructions

---
+++ application/admin-client/src/pages/surveys/getInstrumentFromRedcap.tsx
@@ -0,0 +1,11 @@
+export const instrumentUploadCSVDocumentation = `
+### Survey Instrument Upload
+
+  1. Navigate to \`"Project Home and Design" > "Designer"\` on the sidebar.
+
+     ![designer.png](/import_instruments/designer.png)
+
+  2. Find the Instrument that you want to import to CTRL - Under \`Instrument actions\` , select the \`Choose action\` drop down and click \`Download instrument ZIP\` .
+
+     ![download_instrument_zip.png](/import_instruments/download_instrument_zip.png)
+`
