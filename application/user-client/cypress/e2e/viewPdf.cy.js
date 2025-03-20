8e7232b10a86f7067598b5a13ae497351eed395c
ignatiusm <ignatius.menzies@gmail.com>
2025-03-20 13:32:59 +1300

    Make pdf tests more DRY

--- application/user-client/cypress/e2e/viewPdf.cy.js
+++ application/user-client/cypress/e2e/viewPdf.cy.js
@@ -8,12 +8,13 @@ const { UserType } = require('../support/commands')
 const downloadsPath = 'cypress/downloads/'
 
 describe('viewPdf', () => {
-  it('If unanswered, PDF shows "not answered"', () => {
-    cy.login(UserType.PARTICIPANT_UNANSWERED)
-    cy.visit('/')
-    cy.contains('Intro').should('exist')
-    // Check UI to ensure matches expectations
-    cy.get('[data-cy="step-card-0"]').contains('Requires Review').should('exist')
+  // Function to:
+  //  - click 'View responses' button,
+  //  - access latest downloaded PDF that matches specific Regex
+  //    - (note 'Test User' and date format assumptions)
+  //  - parse PDF
+  //  - check contents include specific text_string parameter.
+  function assertPdfContains(text_string) {
     cy.intercept('GET', '/surveys/responses/current').as('requestPdf')
     cy.get('[data-cy="view-pdf"]').click()
     cy.wait('@requestPdf')
@@ -31,70 +32,38 @@ describe('viewPdf', () => {
 
     cy.get('@pdfFile').then((pdfFile) => {
       cy.readFile(pdfFile).should('exist')
-      // Check PDF
-      cy.task('readPdf', pdfFile).should('contain', 'Question 1 Not answered')
+      // Parse PDF and check contents
+      cy.task('readPdf', pdfFile).should('contain', text_string)
+      // Delete PDF
       cy.task('deleteFile', pdfFile)
     })
+  }
+
+  it('If unanswered, PDF shows "not answered"', () => {
+    cy.login(UserType.PARTICIPANT_UNANSWERED)
+    cy.visit('/')
+    cy.contains('Intro').should('exist')
+    // Check UI to ensure it matches expectations
+    cy.get('[data-cy="step-card-0"]').contains('Requires Review').should('exist')
+    assertPdfContains('Question 1 Not answered')
   })
 
   it('If answered, PDF shows correct answer', () => {
     cy.login(UserType.PARTICIPANT_COMPLETED)
     cy.visit('/')
     cy.contains('Intro').should('exist')
-    // Check UI to ensure matches expectations
+    // Check UI to ensure it matches expectations
     cy.get('[data-cy="step-card-0"]').contains('Reviewed').should('exist')
-    cy.intercept('GET', '/surveys/responses/current').as('requestPdf')
-    cy.get('[data-cy="view-pdf"]').click()
-    cy.wait('@requestPdf')
-    cy.wait(500)
-    cy.task('readDir', 'cypress/downloads')
-      .then((files) => {
-        const regex = /^CTRL-responses-Test_User_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}\.pdf$/
-        const pdfFiles = files.filter((file) => regex.test(file))
-
-        return cy.task('getLatestFile', pdfFiles)
-      })
-      .then((latestFile) => {
-        cy.wrap(`cypress/downloads/${latestFile}`).as('pdfFile')
-      })
-
-    cy.get('@pdfFile').then((pdfFile) => {
-      cy.readFile(pdfFile).should('exist')
-      // Check PDF
-      cy.task('readPdf', pdfFile).should('contain', 'Question 1 No')
-      cy.task('deleteFile', pdfFile)
-    })
+    assertPdfContains('Question 1 No')
   })
 
   it('checks answers, changes them and checks updated answers are updated in PDF', () => {
     cy.login(UserType.PARTICIPANT_COMPLETED)
     cy.visit('/')
     cy.contains('Intro').should('exist')
-    // Check UI to ensure matches expectations
+    // Check UI to ensure it matches expectations
     cy.get('[data-cy="step-card-0"]').contains('Reviewed').should('exist')
-
-    // Check pdf answer
-    cy.intercept('GET', '/surveys/responses/current').as('requestPdf')
-    cy.get('[data-cy="view-pdf"]').click()
-    cy.wait('@requestPdf')
-    cy.wait(500)
-    cy.task('readDir', 'cypress/downloads')
-      .then((files) => {
-        const regex = /^CTRL-responses-Test_User_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}\.pdf$/
-        const pdfFiles = files.filter((file) => regex.test(file))
-
-        return cy.task('getLatestFile', pdfFiles)
-      })
-      .then((latestFile) => {
-        cy.wrap(`cypress/downloads/${latestFile}`).as('pdfFile')
-      })
-
-    cy.get('@pdfFile').then((pdfFile) => {
-      cy.readFile(pdfFile).should('exist')
-      // Check PDF
-      cy.task('readPdf', pdfFile).should('contain', 'Question 2 Choice 2')
-      cy.task('deleteFile', pdfFile)
-    })
+    assertPdfContains('Question 2 Choice 2')
 
     // Change response from above test
     cy.get('[data-cy="step-button-1"]').click()
@@ -103,32 +72,10 @@ describe('viewPdf', () => {
     cy.get('input[type="radio"]').eq(0).should('be.checked')
     cy.contains('Save').click()
     cy.get('[data-cy="proceed-button"]').click()
-
-    // Check response in PDF
-    cy.intercept('GET', '/surveys/responses/current').as('requestPdf')
-    cy.get('[data-cy="view-pdf"]').click()
-    cy.wait('@requestPdf')
-    cy.wait(500)
-    cy.task('readDir', 'cypress/downloads')
-      .then((files) => {
-        const regex = /^CTRL-responses-Test_User_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}\.pdf$/
-        const pdfFiles = files.filter((file) => regex.test(file))
-
-        return cy.task('getLatestFile', pdfFiles)
-      })
-      .then((latestFile) => {
-        cy.wrap(`cypress/downloads/${latestFile}`).as('pdfFile')
-      })
-
-    cy.get('@pdfFile').then((pdfFile) => {
-      cy.readFile(pdfFile).should('exist')
-      // Check PDF
-      cy.task('readPdf', pdfFile).should('contain', 'Question 2 Choice 1')
-      cy.task('deleteFile', pdfFile)
-    })
+    assertPdfContains('Question 2 Choice 1')
 
     // Change response back to original
-    // This updates the reviewed date from that in seed data
+    // This updates the reviewed date to be later that in the seed data
     // Hopefullly this isn't a problem for other tests
     cy.get('[data-cy="step-button-1"]').click()
     cy.get('input[type="radio"]').eq(0).should('be.checked')
