1edce979ff268ac2c810bcdcb4cdeb21358a263a
Peter Louka <p.louka13@gmail.com>
2025-03-20 12:00:27 +1100

    test scripts for admin and user client testing.

---
+++ application/admin-client/scripts/run_tests.sh
@@ -0,0 +1,27 @@
+set -e
+
+# Default mode is 'run' if no argument is provider
+
+if [ "$CYPRESS_MODE" != "open" ] && [ "$CYPRESS_MODE" != "run" ]; then
+    echo "Invalid mode. Use 'open' or 'run'"
+    exit 1
+fi
+
+echo "Running tests in cypress $CYPRESS_MODE mode"
+
+# Spin-up backend and db
+docker compose up --build -d --wait db-test
+echo "Waiting for db to spin up..."
+sleep 3 # Wait for db to spin up
+docker compose up --build -d --wait backend-test
+
+# Generate prisma types
+yarn workspace backend prisma:generate
+
+printenv
+
+# run tests
+npx dotenv -e ../../backend/.env.test start-server-and-test 'refine dev --port 5003 --host 0.0.0.0' http://localhost:5003 cy:$CYPRESS_MODE
+
+# Tear down
+docker compose down db-test backend-test
