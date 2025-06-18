5c7afd40508e325734879d46e6e6fdf0c9da196e
ignatiusm <ignatius.menzies@gmail.com>
2025-06-18 16:42:20 +1200

    Fix admin multistudy tests

--- application/admin-client/cypress/e2e/multiStudy.cy.js
+++ application/admin-client/cypress/e2e/multiStudy.cy.js
@@ -24,12 +24,20 @@ describe('Multi-study features', () => {
     cy.get('[data-cy="study-dropdown"]').should('have.text', 'TEST')
   })
   it('Can changes studies, all data is updated accordingly', () => {
+    cy.visit('/')
+    cy.get('[data-cy="study-dropdown"]').click()
+    cy.get('[data-cy="new-study-button"]').click()
+    cy.get('[data-cy="study-create"]').click()
+    cy.get('[data-cy="study-name"] input').should('be.focused').type('TEST')
+    cy.get('[data-cy="study-create"]').click()
     //List of surveys
+    changeStudy('Test Study')
     cy.visit('/surveys')
     cy.get('[role="rowgroup"]').children().should('have.length', 2)
     changeStudy('Study 2')
     cy.get('[role="rowgroup"]').children().should('have.length', 1)
-    //Survey version data is updated correctly
+    //Survey version data is updated correctly (`TEST` has no content)
+    changeStudy('TEST')
     cy.get('[data-cy="edit-draft-button"]').click()
     cy.get('[data-cy="step-list"]').children().should('have.length', 0)
     cy.get('[data-cy="study-dropdown"]').should('be.disabled')
