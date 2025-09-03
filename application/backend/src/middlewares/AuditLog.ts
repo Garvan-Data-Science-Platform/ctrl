3da08219866285ca7ae662cf13ae2a1c8d1d4b4a
Tim Kallady <t.kallady@garvan.org.au>
2025-09-03 13:45:12 +1000

    Add auth to audit log

--- application/backend/src/middlewares/AuditLog.ts
+++ application/backend/src/middlewares/AuditLog.ts
@@ -3,9 +3,9 @@ import { Request, Response, NextFunction } from 'express'
 import prisma from '../PrismaClient'
 
 export async function auditLog(req: Request, res: Response, next: NextFunction) {
-  const userId = req.user?.userId
+  const userId = req.user?.userId || undefined
 
-  if (!userId || !['PATCH', 'DELETE', 'POST', 'PUT'].includes(req.method)) {
+  if (!['PATCH', 'DELETE', 'POST', 'PUT'].includes(req.method)) {
     next()
     return
   }
@@ -40,12 +40,25 @@ export async function auditLog(req: Request, res: Response, next: NextFunction) 
       .filter((val) => !/^-?\d+$/.test(val || '') && val !== 'current' && val)
       .join('/')
     const success = 200 <= res.statusCode && res.statusCode <= 299
-    const meta = { resourceId: id, bodyData: req.body, url: req.url, method: req.method }
+    const bodyData = req.body
+    if (bodyData.password) {
+      bodyData.password = '***'
+    }
+    const meta = { resourceId: id, url: req.url, method: req.method }
 
     try {
-      await prisma.auditLog.create({ data: { userId: userId, operation, resource, meta, success } })
-    } catch {
-      console.log('User not found')
+      await prisma.auditLog.create({
+        data: {
+          userId: userId,
+          operation,
+          resource,
+          meta,
+          requestBody: JSON.stringify(bodyData),
+          success,
+        },
+      })
+    } catch (e) {
+      console.log('Audit log error', e)
     }
   })
 
