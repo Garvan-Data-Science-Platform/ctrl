20db32be955e23ff0bb80909ed9f670b8ccd72f1
Tim Kallady <t.kallady@garvan.org.au>
2025-09-09 10:16:01 +1000

    fix

--- application/backend/prisma/console.ts
+++ application/backend/prisma/console.ts
@@ -1,9 +1,7 @@
 // Set up a REPL for Prisma
 // Prisma client is available as `prisma`
 
-import { PrismaClient } from '@prisma/client'
+import prisma from '../src/PrismaClient'
 
-// Note: this doesn't use `application/backend/src/PrismaClient.ts` with middleware...
-const prisma = new PrismaClient()
-
+//@ts-ignore
 globalThis.prisma = prisma
