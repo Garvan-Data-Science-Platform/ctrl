b834032272e619ef7c0513e578739d40b3008fa3
ignatiusm <ignatius.menzies@gmail.com>
2025-12-17 19:35:12 +1300

    Adds more settings page cypress tests

--- application/admin-client/cypress/e2e/settings.cy.js
+++ application/admin-client/cypress/e2e/settings.cy.js
@@ -78,13 +78,60 @@ describe('Settings page', () => {
     cy.contains('Invalid colour').should('not.exist')
   })
 
-  it('Can update logo', () => {
+  it('Can upload an org logo', () => {
     cy.login(UserType.ADMIN)
     cy.visit('/settings')
     cy.get('[data-cy="logo-upload"]').attachFile('valid_logo.png')
     cy.contains('Updated logo').should('exist')
-    cy.get('[data-cy="logo-preview"]').invoke('prop', 'naturalWidth').should('be.greaterThan', 0)
-    cy.get('[data-cy="logo-preview"]').invoke('prop', 'naturalHeight').should('equal', 85)
+    cy.readFile('../common/testing/fixtures/logo_hashes.json').then((data) => {
+      cy.get('[data-cy="logo-preview"]')
+        .should('be.visible')
+        .and('have.attr', 'src')
+        .then((src) => {
+          cy.request({ url: src, encoding: 'base64' }).then((response) => {
+            cy.task('calculateHash', response.body).then((hash) => {
+              expect(hash).to.equal(data.validLogoResizedHash)
+            })
+          })
+        })
+    })
+  })
+
+  it('Can update an org logo', () => {
+    cy.login(UserType.ADMIN)
+    cy.visit('/settings')
+    // Upload original logo
+    cy.get('[data-cy="logo-upload"]').attachFile('valid_logo.png')
+    cy.contains('Updated logo').should('exist')
+    cy.readFile('../common/testing/fixtures/logo_hashes.json').then((data) => {
+      cy.get('[data-cy="logo-preview"]')
+        .should('be.visible')
+        .and('have.attr', 'src')
+        .then((src) => {
+          cy.request({ url: src, encoding: 'base64' }).then((response) => {
+            cy.task('calculateHash', response.body).then((hash) => {
+              expect(hash).to.equal(data.validLogoResizedHash)
+            })
+          })
+        })
+    })
+
+    // Update logo to another image
+    cy.get('[data-cy="logo-upload"]').attachFile('alternate_logo.png')
+    cy.contains('Updated logo').should('exist')
+    cy.readFile('../common/testing/fixtures/logo_hashes.json').then((data) => {
+      cy.get('[data-cy="logo-preview"]')
+        .should('be.visible')
+        .and('have.attr', 'src')
+        .then((src) => {
+          cy.request({ url: src, encoding: 'base64' }).then((response) => {
+            cy.task('calculateHash', response.body).then((hash) => {
+              cy.writeFile('cypress/debug_logo.png', response.body, 'base64')
+              expect(hash).to.equal(data.alternateLogoResizedHash)
+            })
+          })
+        })
+    })
   })
 
   it('Invalid logo fails to update', () => {
@@ -92,5 +139,31 @@ describe('Settings page', () => {
     cy.visit('/settings')
     cy.get('[data-cy="logo-upload"]').attachFile('invalid_logo.png')
     cy.contains('Failed').should('exist')
+    cy.get('[data-cy="logo-preview"]').should('not.exist')
+  })
+
+  it('Can delete an org Logo', () => {
+    cy.login(UserType.ADMIN)
+    cy.visit('/settings')
+    // Upload a logo to delete
+    cy.get('[data-cy="logo-upload"]').attachFile('valid_logo.png')
+    cy.contains('Updated logo').should('exist')
+    cy.readFile('../common/testing/fixtures/logo_hashes.json').then((data) => {
+      cy.get('[data-cy="logo-preview"]')
+        .should('be.visible')
+        .and('have.attr', 'src')
+        .then((src) => {
+          cy.request({ url: src, encoding: 'base64' }).then((response) => {
+            cy.task('calculateHash', response.body).then((hash) => {
+              expect(hash).to.equal(data.validLogoResizedHash)
+            })
+          })
+        })
+    })
+
+    // test logo delete
+    cy.get('[data-cy="logo-delete"]').click()
+    cy.contains('Deleted logo').should('exist')
+    cy.get('[data-cy="logo-preview"]').should('not.exist')
   })
 })
