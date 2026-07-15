212d3e8421dec2569605d0db34d0cc060c5d2d4f
ignatiusm <11403879+ignatiusm@users.noreply.github.com>
2026-07-15 21:37:20 +0000

    Constrain User api response (#878)
    * Add tests for password in user endpoints
    
    * Add tests for emailHash in users endpoints
    
    * Add omitApi prisma feature
    
    * update types and docs
    
    * Update users endpoints to use new types and omit pw and emailHash
    
    * Adds test for info exposure via patch user endpoint
    
    * Revert "Adds test for info exposure via patch user endpoint"
    
    This reverts commit 445418319214a1569bccb7260e7246519b3b1c29.
    
    * Remove commented line

--- application/backend/src/controllers/UsersController.test.ts
+++ application/backend/src/controllers/UsersController.test.ts
@@ -76,6 +76,28 @@ describe('UsersController', () => {
       const body: GetAllUsersResponse = response.body
       expect(body.data).toBe(undefined)
     })
+
+    it('should not return user password', async () => {
+      const response = await request(app)
+        .get('/users')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(response.status).toBe(200)
+
+      const body: GetAllUsersResponse = response.body
+      expect(body).toHaveProperty('data')
+      expect(body.data[0]).not.toHaveProperty('password')
+    })
+
+    it('should not return user emailHash', async () => {
+      const response = await request(app)
+        .get('/users')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(response.status).toBe(200)
+
+      const body: GetAllUsersResponse = response.body
+      expect(body).toHaveProperty('data')
+      expect(body.data[0]).not.toHaveProperty('emailHash')
+    })
   })
 
   describe('GET /users/admin', () => {
@@ -90,6 +112,28 @@ describe('UsersController', () => {
       body.data.map((user) => {
         expect(user.role).not.toEqual('Participant')
       })
+    })
+
+    it('should not return admin password', async () => {
+      const response = await request(app)
+        .get('/users')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(response.status).toBe(200)
+
+      const body: GetAllUsersResponse = response.body
+      expect(body).toHaveProperty('data')
+      expect(body.data[0]).not.toHaveProperty('password')
+    })
+
+    it('should not return admin emailHash', async () => {
+      const response = await request(app)
+        .get('/users')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(response.status).toBe(200)
+
+      const body: GetAllUsersResponse = response.body
+      expect(body).toHaveProperty('data')
+      expect(body.data[0]).not.toHaveProperty('emailHash')
     })
   })
 
@@ -117,6 +161,28 @@ describe('UsersController', () => {
       expect(response.status).toBe(404)
 
       expect(response.body.message).toBe(`User with ID: ${userId} not found`)
+    })
+
+    it('should not return user password', async () => {
+      const response = await request(app)
+        .get('/users')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(response.status).toBe(200)
+
+      const body: GetAllUsersResponse = response.body
+      expect(body).toHaveProperty('data')
+      expect(body.data[0]).not.toHaveProperty('password')
+    })
+
+    it('should not return user emailHash', async () => {
+      const response = await request(app)
+        .get('/users')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(response.status).toBe(200)
+
+      const body: GetAllUsersResponse = response.body
+      expect(body).toHaveProperty('data')
+      expect(body.data[0]).not.toHaveProperty('emailHash')
     })
   })
 
@@ -582,6 +648,44 @@ describe('UsersController', () => {
       expect(res2.ok).toBe(true)
       expect(res2.body.data).toHaveLength(1)
       expect(res2.body.data[0].id).toBe(OPERATOR_ADMIN_ID)
+    })
+
+    it('should not return deleted admin password', async () => {
+      const res1 = await request(app)
+        .get('/users/admin/deleted')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(res1.ok).toBe(true)
+      expect(res1.body.data).toHaveLength(0)
+      await prisma.user.delete({
+        where: {
+          id: OPERATOR_ADMIN_ID,
+        },
+      })
+      const res2 = await request(app)
+        .get('/users/admin/deleted')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(res2.ok).toBe(true)
+      expect(res2.body.data).toHaveLength(1)
+      expect(res2.body.data[0]).not.toHaveProperty('password')
+    })
+
+    it('should not return deleted admin emailHash', async () => {
+      const res1 = await request(app)
+        .get('/users/admin/deleted')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(res1.ok).toBe(true)
+      expect(res1.body.data).toHaveLength(0)
+      await prisma.user.delete({
+        where: {
+          id: OPERATOR_ADMIN_ID,
+        },
+      })
+      const res2 = await request(app)
+        .get('/users/admin/deleted')
+        .set({ Authorization: `Bearer ${orgAdminToken}` })
+      expect(res2.ok).toBe(true)
+      expect(res2.body.data).toHaveLength(1)
+      expect(res2.body.data[0]).not.toHaveProperty('emailHash')
     })
   })
 
--- application/common/types/api/users/getUserById.ts
+++ application/common/types/api/users/getUserById.ts
@@ -1,5 +1,5 @@
-import { User } from '@prisma/client'
+import { UserResponse } from './getAllUsers'
 
 export interface GetUserByIdResponse {
-  data: User
+  data: UserResponse
 }
