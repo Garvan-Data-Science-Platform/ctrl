5c76e20e1419368f85d131686f7662abd782abe2
ignatiusm <ignatius.menzies@gmail.com>
2026-08-26 20:11:53 +1200

    Add admin-client xss test for setup page

--- application/admin-client/cypress/e2e/setup.cy.js
+++ application/admin-client/cypress/e2e/setup.cy.js
@@ -13,4 +13,29 @@ describe('Setup', () => {
     cy.visit('/surveys')
     cy.contains('Current Draft').should('exist')
   })
+
+  it('Cannot register with xss', () => {
+    cy.task('wipe')
+    cy.visit('/')
+    cy.url().should('contain', '/setup')
+    cy.get('[data-cy="setup-email"]').type(
+      "{{7*7}}<script>alert('xss-dep-first')</script>@gmail.com",
+      {
+        parseSpecialCharSequences: false,
+      },
+    )
+    cy.get('[data-cy="setup-password"]').type(TestUsers.ORG_ADMIN.password) // Using test data to conform to pr requirements
+    cy.get('[data-cy="setup-submit"]').click()
+    cy.contains('Enter a valid email').should('exist')
+  })
+
+  it('Cannot register with weak password', () => {
+    cy.task('wipe')
+    cy.visit('/')
+    cy.url().should('contain', '/setup')
+    cy.get('[data-cy="setup-email"]').type('abc@d.com')
+    cy.get('[data-cy="setup-password"]').type('password')
+    cy.get('[data-cy="setup-submit"]').click()
+    cy.contains('Invalid password').should('exist')
+  })
 })
--- application/admin-client/src/pages/setup/index.tsx
+++ application/admin-client/src/pages/setup/index.tsx
@@ -3,7 +3,7 @@ import { Alert, Box, Button, Card, Container, TextField, Typography } from '@mui
 import { useLogin } from '../../hooks/useLogin'
 import { useForm } from 'react-hook-form'
 import { checkPasswordStrength } from '@common/src/PasswordStrength'
-import { REGEX } from '@common/types/commonTypes'
+import { emailRules } from '@common/src/validation'
 
 export const SetupPage = () => {
   const {
@@ -69,13 +69,7 @@ export const SetupPage = () => {
               data-cy="setup-email"
               error={Boolean(errors.email)}
               helperText={errors.email?.message as any}
-              {...register('email', {
-                required: true,
-                pattern: {
-                  value: REGEX.EMAIL,
-                  message: 'Enter a valid email',
-                },
-              })}
+              {...register('email', emailRules())}
             />
             <TextField
               type="password"
