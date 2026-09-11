039a4d6829bdb1487011166032943138ea1db717
ignatiusm <ignatius.menzies@gmail.com>
2026-09-11 13:40:59 +1200

    Change cypress test scroll to work in CI and locally

--- application/admin-client/cypress/e2e/redcapParticipantUpload.cy.js
+++ application/admin-client/cypress/e2e/redcapParticipantUpload.cy.js
@@ -252,14 +252,14 @@ describe('REDCap Participant Upload', () => {
       // date of birth
       cy.contains('xss-dob').should('be.visible').realHover({ scrollBehavior: 'center' })
       cy.contains(VALIDATION_MESSAGES.DOB_INVALID).should('be.visible')
-      // mobile
-      cy.contains('xss-mobile').should('be.visible').realHover({ scrollBehavior: 'center' })
-      cy.contains(VALIDATION_MESSAGES.MOBILE_INVALID).should('be.visible')
-      // address (need to scroll to the right first)
-      cy.contains('xss-address')
+      // mobile (need to scroll to the right first)
+      cy.contains('xss-mobile')
         .scrollIntoView({ block: 'center', inline: 'center' })
         .should('be.visible')
-        .realHover()
+        .realHover({ scrollBehavior: 'center' })
+      cy.contains(VALIDATION_MESSAGES.MOBILE_INVALID).should('be.visible')
+      // address
+      cy.contains('xss-address').should('be.visible').realHover()
       cy.contains(VALIDATION_MESSAGES.ADDRESS_INVALID).should('be.visible')
       // suburb
       cy.contains('xss-suburb').should('be.visible').realHover({ scrollBehavior: 'center' })
@@ -267,14 +267,14 @@ describe('REDCap Participant Upload', () => {
       // postcode
       cy.contains('xss-postcode').should('be.visible').realHover({ scrollBehavior: 'center' })
       cy.contains(VALIDATION_MESSAGES.POSTCODE_INVALID).should('be.visible')
-      // next of kin (nok) first name
-      cy.contains('xss-nok-first-name').should('be.visible').realHover({ scrollBehavior: 'center' })
-      cy.contains(VALIDATION_MESSAGES.NAME_INVALID).should('be.visible')
-      // nok last name (need to scroll to the right again)
-      cy.contains('xss-nok-last-name')
+      // next of kin (nok) first name (need to scroll to the right again)
+      cy.contains('xss-nok-first-name')
         .scrollIntoView({ block: 'center', inline: 'center' })
         .should('be.visible')
         .realHover({ scrollBehavior: 'center' })
+      cy.contains(VALIDATION_MESSAGES.NAME_INVALID).should('be.visible')
+      // nok last name
+      cy.contains('xss-nok-last-name').should('be.visible').realHover({ scrollBehavior: 'center' })
       cy.contains(VALIDATION_MESSAGES.NAME_INVALID).should('be.visible')
       // nok email
       cy.contains('xss-nok-email').should('be.visible').realHover({ scrollBehavior: 'center' })
